fe075ac800cff3cd5528020006b4d378cd0d59a5
sandeep <sandeepch41@gmail.com>
2018-10-29 22:24:08 -0500

    h
    ch

--- src/main/java/com/mavsgold/payments/service/impl/ChargesServiceImpl.java
+++ src/main/java/com/mavsgold/payments/service/impl/ChargesServiceImpl.java
@@ -18,7 +18,7 @@ public class ChargesServiceImpl implements ChargesService {
 
         // Set your secret key: remember to change this to your live secret key in production
         // See your keys here: https://dashboard.stripe.com/account/apikeys
-        Stripe.apiKey = "sk_test_SXds7vVfDwWaG7AaJApFwOlF";
+        Stripe.apiKey = "sk_test_SXdslF";
 
 
         String token = "tok_1DPzKaEcAcnWKXu1RezEgxtg";
